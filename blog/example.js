caf52434687b1557970ceddcf555043e74af27db
Hyunsoo An <hankjudolove@gmail.com>
2024-06-21 16:15:23 +1200

    adding java and DOM page and fixing CSS

--- blog/example.js
+++
@@ -1,9 +0,0 @@
-let Hank = {
-  fullname: 'Hyunsoo An',
-  age: 21,
-  city: 'Tauranga',
-}
-
-console.log(Hank.fullname) // Result Hyunsoo An
-console.log(Hank['age']) // Result 21
-console.log(Hank.city) // Result Tauranga

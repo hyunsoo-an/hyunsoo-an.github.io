4d23635832f1460560c67b9ab32726fb3095b85d
Hyunsoo An <hankjudolove@gmail.com>
2024-06-21 15:56:12 +1200

    explain Javascritp and DOM

---
+++ blog/example.js
@@ -0,0 +1,9 @@
+let Hank = {
+  fullname: 'Hyunsoo An',
+  age: 21,
+  city: 'Tauranga',
+}
+
+console.log(Hank.fullname) // Result Hyunsoo An
+console.log(Hank['age']) // Result 21
+console.log(Hank.city) // Result Tauranga
